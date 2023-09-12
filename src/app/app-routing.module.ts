2a79e6b51eb8a4c1104cba8a82dde9b763e989e2
Terry <109131914+tkterry256@users.noreply.github.com>
2023-09-12 20:04:46 +0300

    Formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PaymentComponent } from './payment/payment.component';
 import { PaymentSuccessComponent } from './payment-success/payment-success.component';
 
-const routes: Routes = [{path: '', component: PaymentComponent}, {path: 'payment-success', component: PaymentSuccessComponent}];
+const routes: Routes = [
+  { path: '', component: PaymentComponent },
+  { path: 'payment-success', component: PaymentSuccessComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
